2f1f7cb89a3f63460e0c47dfed45974fcd03923f
M Saravanan <saravananm1912@gmail.com>
2026-07-23 09:04:32 +0530

    Delete Big Data directory

--- Big Data/partitioner.py
+++
@@ -1,8 +0,0 @@
-def partition(data, number_of_partitions):
-    partitions = [[] for i in range(number_of_partitions)]
-
-    for key, value in data:
-        position = hash(key) % number_of_partitions
-        partitions[position].append((key, value))
-
-    return partitions
